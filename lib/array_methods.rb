8213ee97d4c2c9373f8f2b11907da2da970ea5a7
Kenneth Portillo <portillokenneth@gmail.com>
2019-08-19 22:24:08 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,7 +1,7 @@
 def using_include(array, element)
   array = ["wow", "I", "am", "really", "learning", "arrays!"]
   array.include?("wow")
-  array.include?("wohw")
+  array.include?("wow")
 end
 
 def using_sort(array)
